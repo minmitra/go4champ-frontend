179725190a58a9a40399c0d24efe1939ec7758f6
Asl43n <aslan.sohel@yahoo.com>
2025-05-06 15:38:18 +0200

    BE LogReg

---
+++ src/main/java/com/go4champ/go4champ/Go4ChampApplication.java
@@ -0,0 +1,13 @@
+package com.go4champ.go4champ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Go4ChampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Go4ChampApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/go4champ/go4champ/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.go4champ.go4champ.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.go4champ.go4champ.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, String> {
+}
