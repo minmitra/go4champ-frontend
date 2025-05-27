e10dd16920b1864783cc10e4bea8fe9fe6838400
suas4491 <suas4491@bht-berlin.de>
2025-05-27 20:08:48 +0200

    Update 22 files
    - /backend/target/classes/com/go4champ/go4champ/config/CorsConfig$1.class
    - /backend/target/classes/com/go4champ/go4champ/config/CorsConfig.class
    - /backend/target/classes/com/go4champ/go4champ/controller/LoginController.class
    - /backend/target/classes/com/go4champ/go4champ/controller/RegistrationController.class
    - /backend/target/classes/com/go4champ/go4champ/controller/UserController.class
    - /backend/target/classes/com/go4champ/go4champ/model/Training.class
    - /backend/target/classes/com/go4champ/go4champ/model/User.class
    - /backend/target/classes/com/go4champ/go4champ/repo/UserRepo.class
    - /backend/target/classes/com/go4champ/go4champ/service/UserService.class
    - /backend/target/classes/com/go4champ/go4champ/Go4ChampApplication.class
    - /backend/src/main/java/com/go4champ/go4champ/config/CorsConfig.java
    - /backend/src/main/java/com/go4champ/go4champ/controller/LoginController.java
    - /backend/src/main/java/com/go4champ/go4champ/controller/RegistrationController.java
    - /backend/src/main/java/com/go4champ/go4champ/controller/UserController.java
    - /backend/src/main/java/com/go4champ/go4champ/model/Training.java
    - /backend/src/main/java/com/go4champ/go4champ/model/User.java
    - /backend/src/main/java/com/go4champ/go4champ/repo/UserRepo.java
    - /backend/src/main/java/com/go4champ/go4champ/service/UserService.java
    - /backend/src/main/java/com/go4champ/go4champ/Go4ChampApplication.java
    - /backend/mvnw
    - /backend/mvnw.cmd
    - /backend/pom.xml

--- backend/src/main/java/com/go4champ/go4champ/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.go4champ.go4champ.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.go4champ.go4champ.model.User;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, String> {
-}
