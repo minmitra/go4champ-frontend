32ccce901a53265761c88acc68ff0eb75fa573d4
Dein Name <deine@email.com>
2025-05-28 14:02:51 +0200

    Merge branch 'feature/JWTnConf' of gitlab.bht-berlin.de:fitness-security/go4champion into feature/JWTnConf
    # Conflicts:
    #	data/mydb.mv.db
    fertig stellung von pw/email bestätitgung vergessen usw

--- src/main/java/com/go4champ/go4champ/repo/PasswordResetTokenRepo.java
+++ src/main/java/com/go4champ/go4champ/repo/PasswordResetTokenRepo.java
@@ -1,5 +1,3 @@
-// Speichern unter: src/main/java/com/go4champ/go4champ/repo/PasswordResetTokenRepo.java
-
 package com.go4champ.go4champ.repo;
 
 import com.go4champ.go4champ.model.PasswordResetToken;
@@ -7,14 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 @Repository
 public interface PasswordResetTokenRepo extends JpaRepository<PasswordResetToken, Long> {
+
     Optional<PasswordResetToken> findByToken(String token);
-    Optional<PasswordResetToken> findByEmail(String email);
 
     @Modifying
+    @Transactional
     @Query("DELETE FROM PasswordResetToken p WHERE p.email = ?1")
     void deleteByEmail(String email);
 }
